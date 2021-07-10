24ffc28306294d62f4348390e7637a45ba15da9e
kehan2008 <83398000+kehan2008@users.noreply.github.com>
2021-07-10 12:35:21 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,36 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.image = loadImage("block.png");
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed < 3){
+        var pos= this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width, this.height);
+        push();
+        translate(pos.x,pos.y)
+        rotate(this.polygon)
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height)
+        pop();
+
+      }else{
+
+        World.remove(world,this.body);
+        push();
+        this.visibility = this.visibility-5;
+        tint(255,this.visibility);
+        image(this.image,0,0,this.width,this.height);
+      pop();
+
+      }
+    }
+}
